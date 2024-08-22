c43ea41f99514a6d5958ba0c368e68575611a664
GideonBature <infoaboutgideon@gmail.com>
2024-08-22 04:31:22 +0100

    add gitignore file

---
+++ data_types/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let decimal = 25_456;
+    let hex = 0xdab;
+    let octal = 0o77;
+    let binary = 0b1010_1010;
+    let byte = b'g';
+
+    println!("decimal: {}, \nhex: {}, \noctal: {}, \nbinary: {}, \nbyte: {}", decimal, hex, octal, binary, byte);
+}
