2f1a18c41b01832316b3e550272becc5f1699370
GideonBature <infoaboutgideon@gmail.com>
2024-08-26 10:48:10 +0100

    finish

---
+++ enums/src/main.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    v4,
+    v6,
+}
+
+#[derive(Debug)]
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+#[derive(Debug)]
+struct Traffic {
+    light: TrafficLight,
+    action: String,
+}
+
+fn main() {
+    let four = IpAddrKind::v4;
+    let six = IpAddrKind::v6;
+
+    println!("IPv4: {:?}\nIPv6: {:?}", four, six);
+
+    let traffic_stop = TrafficLight::Red;
+    action(traffic_stop);
+
+    let traffic_go = TrafficLight::Green;
+    action(traffic_go);
+
+    let traffic_ready = TrafficLight::Yellow;
+    action(traffic_ready);
+
+    // delay for a second
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let traffic1 = Traffic {
+        light: TrafficLight::Red,
+        action: String::from("Stop"),
+    };
+    println!("\n{}", traffic1.action);
+
+    // delay for a second
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let traffic2 = Traffic {
+        light: TrafficLight::Yellow,
+        action: String::from("Get ready to move"),
+    };
+    println!("\n{}", traffic2.action);
+
+    // delay for a second
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let traffic3 = Traffic {
+        light: TrafficLight::Green,
+        action: String::from("Go"),
+    };
+    println!("\n{}", traffic3.action);
+
+    // delay for a second
+    std::thread::sleep(std::time::Duration::from_secs(1));
+}
+
+fn action(traffic_type: TrafficLight) {
+    println!("Traffic light is {:?}", traffic_type);
+}
