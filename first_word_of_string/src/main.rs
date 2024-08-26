222d75ec16f194638606c90828141b5a02e75202
GideonBature <infoaboutgideon@gmail.com>
2024-08-26 05:46:00 +0100

    finish

--- first_word_of_string/src/main.rs
+++ first_word_of_string/src/main.rs
@@ -1,16 +1,37 @@
 fn main() {
-    let s = String::from("Gideon Bature");
-    let word = first_word(&s);
+    let mut s = String::from("Gideon Bature");
+    let word1 = first_word_1(&s);
     println!("s: {}", s);
-    println!("word: {}", word);
+    println!("word: {}", word1);
+
+    let s1 = String::from("Hello, world!");
+    let word2 = first_word_2(&s1);
+    println!("s1: {}", s1);
+    println!("word: {}", word2);
+
+    s.clear();
+    let word3 = first_word_1(&s);
+    println!("s: {}", s);
+    println!("word: {}", word3);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_1(s: &str) -> usize {
     let bytes = s.as_bytes();
+    println!("bytes: {:?}", bytes);
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return i;
         }
     }
     s.len()
+}
+
+fn first_word_2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, item) in bytes.iter().enumerate() {
+        if item == &b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
