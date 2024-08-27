c474f45cfa807b75916ed6dfe92d7436b82c8593
GideonBature <infoaboutgideon@gmail.com>
2024-08-27 05:24:15 +0100

    finish

---
+++ restaurant/src/main.rs
@@ -0,0 +1,44 @@
+mod restaurant {
+    pub mod front_of_house {
+
+        pub mod hosting {
+
+            pub fn add_to_waitlist() {
+                println!("Added to waitlist");
+            }
+
+            pub fn seat_at_table() {
+                println!("Seated at table");
+            }
+        }
+
+        pub mod serving {
+                
+                pub fn take_order() {
+                    println!("Took order");
+                }
+    
+                pub fn serve_order() {
+                    println!("Served order");
+                }
+    
+                pub fn take_payment() {
+                    println!("Took payment");
+                }
+        }
+    }
+
+    mod back_of_house {
+        fn prepare_food() {}
+
+        fn wash_dishes() {}
+    }
+}
+
+fn main() {
+    restaurant::front_of_house::hosting::add_to_waitlist();
+    restaurant::front_of_house::hosting::seat_at_table();
+    restaurant::front_of_house::serving::take_order();
+    restaurant::front_of_house::serving::serve_order();
+    restaurant::front_of_house::serving::take_payment();
+}
