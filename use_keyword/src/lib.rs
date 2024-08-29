666dab140680376a611f5729a0ed12eac6ae2b0a
GideonBature <infoaboutgideon@gmail.com>
2024-08-29 09:53:58 +0100

    finished

---
+++ use_keyword/src/lib.rs
@@ -0,0 +1,14 @@
+mod front_of_house {
+
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("Added to waitlist");
+        }
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
