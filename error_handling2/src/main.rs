c55b74d069dc6bb3b17ee5340d7a2947ead89639
GideonBature <infoaboutgideon@gmail.com>
2024-09-01 01:31:08 +0100

    finished

--- error_handling2/src/main.rs
+++ error_handling2/src/main.rs
@@ -2,15 +2,24 @@ use::std::fs::File;
 use::std::io::ErrorKind;
 
 fn main() { 
-    let name_from_file = File::open("name.txt");
+    // let name_from_file = File::open("name.txt");
 
-    match name_from_file {
-        Ok(file) => println!("File: {:?}", file),
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("name.txt") {
-                Ok(fc) => println!("File created: {:?}", fc),
-                Err(e) => println!("Error creating file: {:?}", e),
-            }
-        },
-    }
+    // match name_from_file {
+    //     Ok(file) => println!("File: {:?}", file),
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("name.txt") {
+    //             Ok(fc) => println!("File created: {:?}", fc),
+    //             Err(e) => println!("Error creating file: {:?}", e),
+    //         }
+    //         other_error => println!("Other error: {:?}", other_error),
+    //     },
+    // }
+
+    // let file = File::open("items.txt").unwrap();
+    // println!("File: {:?}", file);
+
+    let file = File::open("items.txt").expect("Error opening file");
+    println!("File: {:?}", file);
+
+
 }
