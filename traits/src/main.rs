7a3cc5a087344ae283b578a32acaedef1987ef81
GideonBature <infoaboutgideon@gmail.com>
2024-09-07 15:56:32 +0100

    traits

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,50 @@
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("Read more...")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Tasmania"),
+        author: String::from("Iceburgh"),
+        content: String::from("The penguins,ventus and cloud-capade of tasmania \
+        are here to win!"),
+    };
+    println!("1 new article: {}", article.summarize());
 }
