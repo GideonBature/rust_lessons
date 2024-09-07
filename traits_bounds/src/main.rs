b0a511bbab4cb8591ac54340b9b09a9eaaca8050
GideonBature <infoaboutgideon@gmail.com>
2024-09-07 17:07:38 +0100

    traits bounds

---
+++ traits_bounds/src/main.rs
@@ -0,0 +1,102 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{} ({})", self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify(item1: &impl Summary, item2: &impl Summary) {
+//     println!("Breaking news! {} and {}", item1.summarize(), item2.summarize());
+// }
+
+// pub fn notify<T: Summary>(item1: &T, item2: &T) {
+//     println!("Breaking news! {} and {}", item1.summarize(), item2.summarize());
+// }
+
+// use std::fmt::{Display, Debug};
+
+// pub fn notify<T: Summary + Display>(item: &T) {
+
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+//     0
+// }
+
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+//     0
+// }
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn main() {
+    let news_article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Tasmania"),
+        author: String::from("Iceburgh"),
+        content: String::from("The penguins,ventus and cloud-capade of tasmania \
+        are here to win!"),
+    };
+    println!("1. {}", news_article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+    // println!("2. {}", tweet.summarize());
+    notify(&tweet);
+
+    // returns_summarizable().summarize();
+    println!("3. {}", returns_summarizable().summarize());
+
+}
