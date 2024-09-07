92a862a688faa72af2228bd4365391253619f3b4
GideonBature <infoaboutgideon@gmail.com>
2024-09-07 14:01:04 +0100

    generic enums

--- generics_enums/src/main.rs
+++ generics_enums/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+#[derive(Debug)]
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a_name = Option::Some(String::from("Gideon Bature"));
+    let no_name: Option<String> = Option::None;
+
+    println!("a_name: {:#?}", a_name);
+    println!("no_name: {:#?}", no_name);
+
+    let success: Result<String, String> = Result::Ok(String::from("Success"));
+    let error: Result<String, String> = Result::Err(String::from("Error"));
+    println!("success: {:#?}", success);
+    println!("error: {:#?}", error);
 }
