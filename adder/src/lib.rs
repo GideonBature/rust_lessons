82f664ce4f1673aecd91d4677b6b390d42d5574d
GideonBature <infoaboutgideon@gmail.com>
2024-09-08 05:31:04 +0100

    integration tests

---
+++ adder/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn add_two(x: i32) -> i32 {
+    internal_adder(x, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
